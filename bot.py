c15e5ab2d0cee1f5261cf573740614c0d5125495
Cdev <colindarroch.junk@gmail.com>
2026-04-09 06:27:43 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,14 +54,7 @@ async def ban(ctx, member: discord.Member, *, reason: str = "no reason given"):
     await member.ban(reason=reason)
     await ctx.send(f"Banned {member.mention}. Reason: {reason}. They're gone now")
 
-path = "g.gif"
-@bot.command()
-async def g(ctx):
-    Person = ctx.author.mention
-    await ctx.message.delete()
-    file = discord.File(path)
-    await ctx.send(file=file, delete_after=50)
-    await ctx.send(f"Sent by {Person}", delete_after=50)
+
 
 # meme2
 @bot.command()
